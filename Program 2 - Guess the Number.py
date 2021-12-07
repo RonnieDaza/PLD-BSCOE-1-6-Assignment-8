925e826d413c012259ba51607f079bab540ddc11
unknown <daza.ronnie.09212002@gmail.com>
2021-12-07 20:30:47 +0800

    Import random Commit

---
+++ Program 2 - Guess the Number.py
@@ -0,0 +1,10 @@
+# Program 2: Guess the Number
+# Generate 1 random number (0-100).
+# Ask the user to guess the number.
+# Display "Greater than" if the inputted number is greater than the random number.
+# Display "Less than" if the inputted number is less than the random number.
+# Repeat asking the user until the random number has been guessed correctly.
+
+# Steps
+# 1. Import random.
+import random
