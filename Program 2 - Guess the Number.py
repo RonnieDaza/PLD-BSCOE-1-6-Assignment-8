be8a9d7fcf6921402bcffa5a7b75a8e0a758f6d7
unknown <daza.ronnie.09212002@gmail.com>
2021-12-07 20:57:15 +0800

    Display the randomNumber Commit

--- Program 2 - Guess the Number.py
+++ Program 2 - Guess the Number.py
@@ -40,3 +40,5 @@ while userInput != randomNumber:
 print("You guessed the random number right. Congratulations!")
 # 14. Display the userInput.
 print(f"The number you entered is: {userInput}.")
+# 15. Display the randomNumber.
+print(f"The random number is: {randomNumber}.")
