baff922b0969eec399cbc276074b15099bbe3967
unknown <daza.ronnie.09212002@gmail.com>
2021-12-07 19:41:13 +0800

    Import random Commit

---
+++ Program 1 - Lottery.py
@@ -0,0 +1,12 @@
+# Program 1: Lottery
+# Create a program that asks 3 numbers (0-9) from the user.
+# Generate 3 random winning numbers (0-9).
+# Display "Winner" if all 3 input numbers matched the generated numbers.
+# Display "You lose" if not.
+# Display "Try Again y/n" after each game.
+# If the user enter "y" the user will play again.
+# If the user enter "n" the program will exit.
+
+# Steps
+# 1. Import random.
+import random
